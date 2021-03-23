70d53ebc57c6be28d4ec374019c59cdacee98d56
DMuszynski <d.muszynski112@gmail.com>
2021-03-23 23:55:53 +0100

    Add required dependency and project configuration

--- src/main/java/pl/dmuszynski/javatestframeworkcomparison/JavaTestFrameworkComparisonApplication.java
+++ src/main/java/pl/dmuszynski/javatestframeworkcomparison/JavaTestFrameworkComparisonApplication.java
@@ -1,4 +1,11 @@
 package pl.dmuszynski.javatestframeworkcomparison;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class JavaTestFrameworkComparisonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTestFrameworkComparisonApplication.class);
+    }
 }

7dfbb9bf6c42679479b2d6d922d758da7e0db51f
DMuszynski <d.muszynski112@gmail.com>
2021-04-13 19:38:27 +0200

    Add UserRepository file

--- src/main/java/pl/dmuszynski/javatestframeworkcomparison/repository/UserRepository.java
+++ src/main/java/pl/dmuszynski/javatestframeworkcomparison/repository/UserRepository.java
@@ -1,2 +1,10 @@
-package pl.dmuszynski.javatestframeworkcomparison.repository;public interface UserRepository {
+package pl.dmuszynski.javatestframeworkcomparison.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import pl.dmuszynski.javatestframeworkcomparison.model.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
 }

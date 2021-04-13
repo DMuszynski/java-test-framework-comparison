e2aabbcbd04181c35a57e4bde687b6213a4dbd2c
Damian Muszyński <dmuszynski112@gmail.com>
2021-04-14 01:22:29 +0200

    Add Token entity class

---
+++ src/main/java/pl/dmuszynski/javatestframeworkcomparison/model/Token.java
@@ -0,0 +1,29 @@
+package pl.dmuszynski.javatestframeworkcomparison.model;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "TOKEN", schema = "SYS")
+public class Token extends AbstractEntity {
+
+    @NotNull
+    @Column(name = "value", unique = true, length = 40)
+    private String value;
+
+    @MapsId
+    @OneToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    public Token() {}
+
+    public Token(Long id, String value) {
+        super(id);
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}

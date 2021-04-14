d6b3d1b20d1742230c37c5ec42b34bca4e41e6a2
Damian Muszyński <dmuszynski112@gmail.com>
2021-04-14 02:52:47 +0200

    Add UserDetailsServiceImpl class file

---
+++ src/main/java/pl/dmuszynski/javatestframeworkcomparison/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package pl.dmuszynski.javatestframeworkcomparison.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import pl.dmuszynski.javatestframeworkcomparison.repository.UserRepository;
+
+@Service(value = "userDetailsService")
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return this.userRepository.findByUsername(username)
+                .orElseThrow(()-> new UsernameNotFoundException("User Not Found with username: " + username));
+    }
+}
